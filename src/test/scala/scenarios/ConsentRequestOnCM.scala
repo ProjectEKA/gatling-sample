a4504f54f4250741f1012a9a24b4528bbfb14b60
MD Dubey <mddubey409@gmail.com>
2021-03-24 18:18:23 +0530

    Dubey | 000 | Use feader to generate random uuids

--- src/test/scala/scenarios/ConsentRequestOnCM.scala
+++ src/test/scala/scenarios/ConsentRequestOnCM.scala
@@ -7,26 +7,13 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
+import scenarios.CreateConsentRequestOnCMForAPatient.UuidFeeder
 import utils.Environment._
 
 
 object ConsentRequestOnCM {
 
   val gatewaySessionBody = "{\n    \"clientId\": \"" + GATEWAY_CLIENT + "\",\n    \"clientSecret\": \"" + GATEWAY_SECRET + "\",\n    \"grantType\": \"client_credentials\"\n}"
-  val consentRequestOnCMBody = "{\"requestId\": \"" + UUID.randomUUID().toString + "\",\"timestamp\": \"" + LocalDateTime.now(ZoneOffset.UTC) + "\"," +
-    "\"consent\":{\"" +
-    "purpose\":{\"text\":\"Care Managerment\",\"code\":\"CAREMGT\"},\"" +
-    "patient\":{\"id\":\"mdubey@sbx\"},\"" +
-    "hiu\":{\"id\":\"10000002\",\"name\":\"Project EKA HIU\"},\"" +
-    "requester\":{\"name\":\"Dr Manju\"},\"" +
-    "hiTypes\":[\"Prescription\",\"OPConsultation\",\"DiagnosticReport\",\"DischargeSummary\",\"OPConsultation\"],\"" +
-    "permission\":{\"" +
-    "accessMode\":\"VIEW\",\"" +
-    "dateRange\":{\"from\":\"1992-04-03T10:05:26.352Z\",\"to\":\"2020-08-08T10:05:26.352Z\"},\"" +
-    "dataEraseAt\":\"2025-10-30T12:30:00.352Z\",\"" +
-    "frequency\":{\"unit\":\"HOUR\",\"value\":12,\"repeats\":2" +
-    "}}}}"
-
   val gatewayLogin: ChainBuilder = exec(
     http("create session")
       .post("/gateway/sessions")
@@ -38,21 +25,41 @@ object ConsentRequestOnCM {
   //Replace below with a session token from GW
   val GW_ACCESS_TOKEN = "";
 
-    val createConsentRequest: ChainBuilder = exec(http("Create consent request on CM")
+  object UuidFeeder {
+    val feeder = Iterator.continually(Map("uuid" -> java.util.UUID.randomUUID.toString()))
+  }
+
+
+  private val consnetDetail: String = "\"consent\":{\"" +
+    "purpose\":{\"text\":\"Care Managerment\",\"code\":\"CAREMGT\"},\"" +
+    "patient\":{\"id\":\"mdubey@sbx\"},\"" +
+    "hiu\":{\"id\":\"10000002\",\"name\":\"Project EKA HIU\"},\"" +
+    "requester\":{\"name\":\"Dr Manju\"},\"" +
+    "hiTypes\":[\"Prescription\",\"OPConsultation\",\"DiagnosticReport\",\"DischargeSummary\",\"OPConsultation\"],\"" +
+    "permission\":{\"" +
+    "accessMode\":\"VIEW\",\"" +
+    "dateRange\":{\"from\":\"1992-04-03T10:05:26.352Z\",\"to\":\"2020-08-08T10:05:26.352Z\"},\"" +
+    "dataEraseAt\":\"2025-10-30T12:30:00.352Z\",\"" +
+    "frequency\":{\"unit\":\"HOUR\",\"value\":12,\"repeats\":2" +
+    "}}}}"
+  
+  val createConsentRequest: ChainBuilder = exec(http("Create consent request on CM")
       .post("/cm/v0.5/consent-requests/init")
       .header("Authorization", "Bearer " + GW_ACCESS_TOKEN)
-      .body(StringBody(consentRequestOnCMBody))
+      .body(StringBody("{\"requestId\": \"${uuid}\", \"timestamp\": \"" + LocalDateTime.now(ZoneOffset.UTC) + "\"," +
+        consnetDetail))
       .check(status.is(202))
 //      .check( bodyString.saveAs( "RESPONSE_DATA" ) )
     )
-//      .exec( session => {
-//        println("Some Restful Service Response Body:")
+      .exec( session => {
+        println("Some Restful Service Response Body:")
 //        println(session("RESPONSE_DATA").as[String])
-//        session
-//      })
+        session
+      })
 
 
   val createConsentRequests: ScenarioBuilder =
     scenario("Create Consent Request on CM")
+      .feed(UuidFeeder.feeder)
       .exec(createConsentRequest)
 }
